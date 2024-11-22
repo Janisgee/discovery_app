8d9fd18919e420dd78c1e28571ff84e19f6c2fc9
janisgee <janisgeegee@gmail.com>
2024-11-22 18:53:59 +0800

    Refactioring code and created template for reusing as page background

--- app/nextjs-discovery/src/app/ui/buttons.js
+++ app/nextjs-discovery/src/app/ui/buttons.js
@@ -2,10 +2,8 @@ import Link from 'next/link';
 
 export function Button({ useFor, link, color }) {
   return (
-    <div>
-      <Link href={link}>
-        <button className={`font-space_mono ${color}`}>{useFor}</button>
-      </Link>
-    </div>
+    <Link href={link}>
+      <button className={`font-space_mono ${color}`}>{useFor}</button>
+    </Link>
   );
 }
