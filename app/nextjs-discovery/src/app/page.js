64a7535377b84b260ae2fdc3f071a509d6519652
Janisgee <100589152+Janisgee@users.noreply.github.com>
2024-11-23 13:05:27 +0800

    initial frontend setup (#10)
    * initial frontend setup
    
    * Added sign up form
    
    * Added font awesome
    
    * Added images for development
    
    * Added 3 main font family for application
    
    * Get place from form and redirect to location page

---
+++ app/nextjs-discovery/src/app/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { Button } from '@/app/ui/buttons';
+
+export default function App() {
+  return (
+    <div className='p-20 text-center'>
+      <div className='text-right'>
+        <Button useFor='Sign Up' link='/dashboard/signup' color='btn-grey' />
+      </div>
+      <div className='my-20 '>
+        <h1>Discover Your Side!</h1>
+      </div>
+      <Button useFor='Login' link='/dashboard/home' color='btn-violet' />
+    </div>
+  );
+}
