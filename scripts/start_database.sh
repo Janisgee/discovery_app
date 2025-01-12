76dac27a96f502442bd17b94ee42244feaf6fc9a
Janisgee <100589152+Janisgee@users.noreply.github.com>
2025-01-12 09:54:42 +0800

    fix: postgres user name

--- scripts/start_database.sh
+++ scripts/start_database.sh
@@ -3,7 +3,7 @@
 docker run \
   --name discovery_db \
   -e POSTGRES_PASSWORD=postgres \
-  -e POSTGRES_USER=posgres \
+  -e POSTGRES_USER=postgres \
   -e POSTGRES_DB=discovery_app \
   -p 5433:5432 \
   -v discovery_app_pg_data:/var/lib/postgresql/data \
