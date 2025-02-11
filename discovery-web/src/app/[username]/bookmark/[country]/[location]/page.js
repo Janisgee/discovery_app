519674a9217f1b0aa2250f3865c992cacc9680c9
Janisgee <100589152+Janisgee@users.noreply.github.com>
2025-02-12 00:47:16 +0800

    Feat/bookmark page (#38)
    * Added bookmark function into search place page
    
    * Created bookmark page from user for country and city
    
    * Added bookmark catagory page with place card
    
    * Fixed formatting check

--- discovery-web/src/app/[username]/bookmark/[country]/[location]/page.js
+++ discovery-web/src/app/[username]/bookmark/[country]/[location]/page.js
@@ -1,47 +1,83 @@
 "use client";
 
+import { useState, useEffect } from "react";
+// import { useRouter } from "next/compat/router";
 import { useParams } from "next/navigation";
 import BookmarkTemplate from "@/app/ui/template/bookmarkTemplate";
-import ItemTemplete from "@/app/ui/template/itemTemplate";
-import Link from "next/link";
+
+// import Link from "next/link";
 
 export default function BookmarkLocation() {
+  const [display, setDisplay] = useState(false);
+  const [content, setContent] = useState([]);
   const params = useParams();
   console.log(params);
+
   const bookmarkLocation = params.location.toUpperCase().replaceAll("%20", " ");
+  // console.log(content);
+  const spaceLocation = params.location.replaceAll("%20", " ");
+
+  const fetchAllBookmarkByCity = async () => {
+    const data = {
+      city: spaceLocation,
+    };
+    const request = new Request(
+      "http://localhost:8080/api/getAllBookmarkByCity",
+      {
+        method: "POST", // HTTP method
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(data),
+      },
+    );
+
+    try {
+      const response = await fetch(request);
+      console.log("Response status:", response.status);
+      if (response.ok) {
+        const htmlContent = await response.json(); // Use json() to handle HTML response
+        console.log("Received content:", htmlContent);
+        if (htmlContent != null) {
+          setContent(htmlContent);
+        }
+      } else {
+        if (response.status == 401) {
+          alert(
+            `Please login again as 10 mins session expired without taking action.`,
+          );
+          router.push(`/login`);
+        }
+        console.error(
+          `Error fetching bookmark by city ${bookmarkLocation}:`,
+          response.statusText,
+        );
+      }
+    } catch (error) {
+      console.error(
+        `Error fetching bookmark by city ${bookmarkLocation}:`,
+        error,
+      );
+    }
+  };
+  //
+  useEffect(() => {
+    fetchAllBookmarkByCity();
+    setDisplay(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
-      <BookmarkTemplate place={bookmarkLocation}>
-        <h3 className="py-4 text-center text-gray-500">Attraction</h3>
-        <hr />
-        {/* <div className="pt-8">
-          <Link
-            href={`/dashboard/location/${params.location}/attraction/fremantle%20arts%20centre`}
-          >
-            <ItemTemplete
-              imageSource="/user_img/default.jpg"
-              text="Fremantle Arts Centre"
-            ></ItemTemplete>
-          </Link>
-          <Link
-            href={`/dashboard/location/${params.location}/attraction/fremantle%20arts%20centre`}
-          >
-            <ItemTemplete
-              imageSource="/user_img/default.jpg"
-              text="Fremantle Arts Centre"
-            ></ItemTemplete>
-          </Link>{" "}
-          <Link
-            href={`/dashboard/location/${params.location}/attraction/fremantle%20arts%20centre`}
-          >
-            <ItemTemplete
-              imageSource="/user_img/default.jpg"
-              text="Fremantle Arts Centre"
-            ></ItemTemplete>
-          </Link>
-        </div> */}
-        <p className="pt-8 text-center ">No Bookmark</p>
-      </BookmarkTemplate>
+      {display && (
+        <BookmarkTemplate
+          username={params.username}
+          country={params.country}
+          city={bookmarkLocation}
+          placeList={content.PlaceList != undefined && content.PlaceList}
+        ></BookmarkTemplate>
+      )}
     </div>
   );
 }
--- src/sql/queries/users_bookmark.sql
+++ src/sql/queries/users_bookmark.sql
@@ -11,8 +11,13 @@ SELECT * FROM users_bookmark WHERE place_id = $1 AND user_id = $2 LIMIT 1;
 
 -- name: GetAllUserBookmarkPlaceID :many
 SELECT users_bookmark.place_id, users_bookmark.place_name,users_bookmark.catagory, users_bookmark.place_text, places.country, places.city  FROM users_bookmark
-INNER JOIN places ON place_id = places.id 
+INNER JOIN places ON users_bookmark.place_id = places.id
 WHERE user_id = $1;
+
+-- name: GetUserBookmarkCityInfo :many
+SELECT users_bookmark.place_id, users_bookmark.place_name,users_bookmark.catagory, users_bookmark.place_text, places.country, places.city  FROM users_bookmark
+INNER JOIN places ON users_bookmark.place_id = places.id 
+WHERE user_id = $1 AND places.city=$2;
 
 -- name: DeleteUserBookmark :one
 DELETE FROM users_bookmark WHERE place_id = $1 AND user_id = $2 
